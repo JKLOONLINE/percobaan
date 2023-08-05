e6ecdd9d4c54c3e76643a621f0a1d4c78b50b600
JKLOONLINE <140764159+JKLOONLINE@users.noreply.github.com>
2023-08-05 16:39:50 +0700

    Create gpg.sh

---
+++ gpg.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGREEN='\033[1;92m'      # GREEN
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICdyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGREEN='\033[0;92m'       # GREEN
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+ORANGE='\033[0;33m'
+ijo="\e[92;1m"
+NC='\e[0m'
+clear
+
+echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${putih} ${BGX}                Tools Decrypt & ENCRYPT GPG                 ${NC}"
+echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e " ➣ ${putih}File Name ${NC} (${RED}nama fileyang mau di Encrypt) ${NC}"
+echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+
+select option in $choice; do
+        if [ $REPLY = 1 ];
+then
+        echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+        echo -e " ➣ ${putih}File Name ${NC} (${RED}nama fileyang mau di Encrypt) ${NC}"
+        echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+        read file;
+        gpg -c $file
+        echo "The file has been encrypted"
+else
+        echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+        echo -e " ➣ ${putih}File Name ${NC} (${RED}nama fileyang mau di decrypt) ${NC}"
+        echo -e "${blue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+        read file2;
+        gpg -d $file2
+        echo "The file has been decrypted"
+fi
+
+done
